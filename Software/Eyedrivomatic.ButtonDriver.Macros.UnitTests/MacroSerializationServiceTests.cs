050fc6d0441235d1ae729fa6186a9cac425750c8
General-Fault <github@public.conceptgenesis.com>
2016-07-06 21:29:08 -0700

    Added macro serialization and tests Changed type of Delay setting for macro tasks from TimeSpan to int (makes for a more human readable configuration file).

---
+++ Software/Eyedrivomatic.ButtonDriver.Macros.UnitTests/MacroSerializationServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reflection;
+
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+
+using Eyedrivomatic.ButtonDriver.Macros.Models;
+using ApprovalTests;
+using System.IO;
+
+namespace Eyedrivomatic.ButtonDriver.Macros.UnitTests
+{
+    [TestFixture]
+    public class MacroSerializationServiceTests
+    {
+        [Test]
+        [UseReporter(typeof(DiffReporter))]
+        public void TestSerialize()
+        {
+            var macros = new[] {
+                new UserMacro
+                {
+                    DisplayName = "TestMacro1",
+                    Tasks = new ObservableCollection<Models.MacroTask>
+                    {
+                        new ToggleRelayTask { DisplayName = "Toggle Relay 1 a few times.", Relay = 1, DelayMs = 100, Repeat = 3 },
+                        new DelayTask { DisplayName = "Wait for a second.", DelayTimeMs = 1000 },
+                        new ToggleRelayTask { DisplayName = "Toggle Relay 2 once.", Relay = 2 }
+                    }
+                },
+                new Macros.Models.UserMacro
+                {
+                    DisplayName = "TestMacro2"
+                },
+            };
+
+            var basePath = new Uri(Assembly.GetExecutingAssembly().CodeBase);
+            var filename = Path.Combine(Path.GetDirectoryName(basePath.LocalPath), "TestMacros.config");
+            var serializer = new MacroSerializationService { MacrosPath = filename };
+
+            Approvals.VerifyFile(filename);
+        }
+    }
+}
---
+++ Software/Eyedrivomatic.ButtonDriver.Macros.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Eyedrivomatic.ButtonDriver.Macros.UnitTests")]
+[assembly: AssemblyDescription("Unit Tests for the Macro functionality of the Eyedrivomatic system.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Eyedrivomatic")]
+[assembly: AssemblyProduct("Eyedrivomatic")]
+[assembly: AssemblyCopyright("Copyright Eyedrivomatic ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.*")]
